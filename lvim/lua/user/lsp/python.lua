2f01ab6ef440f7abde0a331657b0cd6d9d72a19c
Jixin Han <kalfazed@gmail.com>
2024-07-03 04:08:13 +0900

    disable flake8

--- lvim/lua/user/lsp/python.lua
+++ lvim/lua/user/lsp/python.lua
@@ -13,7 +13,7 @@ formatters.setup {
 -- setup linters
 local linters = require "lvim.lsp.null-ls.linters"
 linters.setup {
-  { command = "flake8", filetypes = { "python" } },
+  -- { command = "flake8", filetypes = { "python" } },
   {
     -- each linter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
     command = "shellcheck",
