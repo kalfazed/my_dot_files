0ca4c69a44c697fd7d6c201d5082496674a991a6
Jixin Han <kalfazed@gmail.com>
2024-07-11 21:52:50 +0900

    change to peco bottom-up style

--- bash/functions/peco_select_directory.sh
+++ bash/functions/peco_select_directory.sh
@@ -2,7 +2,7 @@
 
 # Function to use peco to select a directory and change into it
 function peco_select_directory() {
-  local path=$(ghq root)/$(ghq list | peco)
+  local path=$(ghq root)/$(ghq list | peco --layout=bottom-up)
   if [ -d "$path" ]; then
     cd "$path"
   else
