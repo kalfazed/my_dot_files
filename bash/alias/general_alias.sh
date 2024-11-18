5174364009545644f30d90d38356a7ecd77f24ca
Jixin Han <kalfazed@gmail.com>
2024-11-18 11:34:03 -0500

    update aliases for git commands

--- bash/alias/general_alias.sh
+++ bash/alias/general_alias.sh
@@ -6,13 +6,16 @@ alias la='ls -A'
 alias sleep='sudo systemctl suspend'
 alias make='make -j64'
 alias gt='git log --graph --pretty=format:"%x09%C(auto) %h %Cgreen %ar %Creset%x09 %C(cyan ul)%an%Creset %x09%C(auto)%s %d"'
+alias gm='git commit -m'
 alias dsort='du -sh -- * | sort -rh'
 alias lm='df -hl -x tmpfs'
 alias pip='python -m pip'
-alias ll='exa --icons --long --no-user --no-time'
+alias ll='exa --icons --long --git'
 alias lla='ll -a'
 alias tmux='tmux -u'
 alias pd="peco_select_directory"
+alias rs="rsync -azh --info=progress2 --no-i-r" # copy to local
+alias rss="rsync -azh --info=progress2 -e \"ssh -T -c aes128-ctr -o Compression=no -x\" --block-size=128K --inplace " # copy to remote
 
 # # Alias vim to neovim or lvim
 if [ "$(type -t lvim)" == "file" ]; then
