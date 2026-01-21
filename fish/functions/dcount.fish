ae30d5e38cfb14685106547d6c1fa7307787d33f
Jixin Han <kalfazed@gmail.com>
2026-01-21 13:56:51 +0900

    make sure dcount can support multil folders

--- fish/functions/dcount.fish
+++ fish/functions/dcount.fish
@@ -1,34 +1,56 @@
-# Get the count of folder
-# Usage: dcount <path>
-#
 function dcount_help
-  echo "Usage: dcount [DIRECTORY]"
-  echo ""
-  echo "Counts the number of folder in the specified DIRECTORY."
-  echo "If no DIRECTORY is provided, it counts files in the current directory."
-  echo ""
-  echo "Options:"
-  echo "  -h, --help    Show this help message and exit."
+    echo "Usage: dcount [DIRECTORY...]"
+    echo ""
+    echo "Counts the number of immediate sub-directories in the specified DIRECTORY(s)."
+    echo "If no DIRECTORY is provided, it counts directories in the current path."
+    echo ""
+    echo "Options:"
+    echo "  -h, --help    Show this help message and exit."
 end
 
 function dcount
-  if test (count $argv) -eq 1
-    if test $argv[1] = "--help" -o $argv[1] = "-h"
-      dcount_help
-      return
+    # 1. Check for help arguments
+    if contains -- -h $argv; or contains -- --help $argv
+        dcount_help
+        return 0
     end
-  end
 
-  if test (count $argv) -eq 0
-    set abs_path (realpath .)
-  else if test (count $argv) -eq 1
-    set abs_path (realpath $argv)
-  else
-    echo $RED"Too many arguments"$CLEAR
-    fcount_help
-    return 1
-  end
+    # 2. Determine targets
+    # If no arguments, target is current dir "."; otherwise, use all provided arguments
+    set -l targets
+    if test (count $argv) -eq 0
+        set targets .
+    else
+        set targets $argv
+    end
 
-  set dir_count (find $argv -maxdepth 1 -type d ! -name "."| wc -l)
-  echo -e $GREENB"$dir_count directories "$CLEAR"in $abs_path"
+    set -l total_count 0
+
+    # 3. Loop through each target directory
+    for target in $targets
+        # Check if directory exists
+        if not test -d $target
+            echo $RED"Error: '$target' is not a directory"$CLEAR
+            continue
+        end
+
+        set -l abs_path (realpath $target)
+
+        # 4. Count directories
+        # -mindepth 1: Excludes the directory itself from the count
+        # -maxdepth 1: Only looks at immediate subfolders (does not go recursive)
+        set -l dir_count (find $target -mindepth 1 -maxdepth 1 -type d | wc -l)
+        
+        # Print result for current directory
+        echo -e $GREENB"$dir_count directories "$CLEAR"in $abs_path"
+        
+        # Add to total
+        set total_count (math $total_count + $dir_count)
+    end
+
+    # 5. Show total if multiple directories were scanned
+    if test (count $targets) -gt 1
+        echo "----------------------------------------"
+        echo -e $GREENB"Total: $total_count directories"$CLEAR
+    end
 end
