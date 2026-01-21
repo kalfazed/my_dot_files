e24ef4594151c848393a42a43d28053e11c82a30
Jixin Han <kalfazed@gmail.com>
2026-01-21 13:53:38 +0900

    make sure fcount can support multil folders

--- fish/functions/fcount.fish
+++ fish/functions/fcount.fish
@@ -1,35 +1,54 @@
-# Get the count of files
-# Usage: fcount <path>
-#
 function fcount_help
-  echo "Usage: fcount [DIRECTORY]"
-  echo ""
-  echo "Counts the number of files recursively in the specified DIRECTORY."
-  echo "If no DIRECTORY is provided, it counts files in the current directory."
-  echo ""
-  echo "Options:"
-  echo "  -h, --help    Show this help message and exit."
+    echo "Usage: fcount [DIRECTORY...]"
+    echo ""
+    echo "Counts the number of files recursively in the specified DIRECTORY(s)."
+    echo "If no DIRECTORY is provided, it counts files in the current directory."
+    echo ""
+    echo "Options:"
+    echo "  -h, --help    Show this help message and exit."
 end
 
 function fcount
-  if test (count $argv) -eq 1
-    if test $argv[1] = "--help" -o $argv[1] = "-h"
-      fcount_help
-      return 1
+    # 1. Check for help arguments
+    if contains -- -h $argv; or contains -- --help $argv
+        fcount_help
+        return 0
     end
-  end
 
-  if test (count $argv) -eq 0
-    set abs_path (realpath .)
-  else if test (count $argv) -eq 1
-    set abs_path (realpath $argv)
-  else
-    echo $RED"Too many arguments"$CLEAR
-    fcount_help
-    return 1
-  end
+    # 2. Determine targets
+    # If no arguments, target is current dir "."; otherwise, use all provided arguments
+    set -l targets
+    if test (count $argv) -eq 0
+        set targets .
+    else
+        set targets $argv
+    end
 
-  set file_count (find $argv -type f | xargs ls | wc -l)
-  echo -e $GREENB"$file_count files "$CLEAR"in $abs_path"
+    set -l total_count 0
+
+    # 3. Loop through each target directory
+    for target in $targets
+        # Check if directory exists
+        if not test -d $target
+            echo $RED"Error: '$target' is not a directory"$CLEAR
+            continue
+        end
+
+        set -l abs_path (realpath $target)
+        
+        # 4. Count files (Optimized: removed 'xargs ls', direct pipe to wc -l is safer/faster)
+        set -l file_count (find $target -type f | wc -l)
+        
+        # Print result for current directory
+        echo -e $GREENB"$file_count files "$CLEAR"in $abs_path"
+        
+        # Add to total
+        set total_count (math $total_count + $file_count)
+    end
+
+    # 5. Show total if multiple directories were scanned
+    if test (count $targets) -gt 1
+        echo "----------------------------------------"
+        echo -e $GREENB"Total: $total_count files"$CLEAR
+    end
 end
-
