082eede87c45172ab13f3ea04f31fdd26800f5d1
Jixin Han <kalfazed@gmail.com>
2026-03-23 04:09:44 +0000

    remove trash

--- nvim/install.sh
+++ nvim/install.sh
@@ -11,12 +11,16 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 source "$HOME/.cargo/env"
 
 # install npm
-sudo apt-get install -y codespell flake8 black isort
+sudo apt-get install -y codespell flake8 black isort unzip
 
 # install neovim
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
+rm nvim-linux-x86_64.tar.gz
 sudo rm -rf /opt/nvim-linux-x86_64
 sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
+
+# install treesitter
+C_INCLUDE_PATH=/usr/lib/gcc/x86_64-linux-gnu/11/include cargo install --locked tree-sitter-cli
 
 mkdir ~/.config/nvim
 cp -r * ~/.config/nvim/
